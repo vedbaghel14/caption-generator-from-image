d65c43a3916f16be02d06b173f9b2abd20120c77
vedbaghel14 <baghelved341@gmail.com>
2026-05-29 04:10:13 +0530

    added frontend in backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://caption-generator-from-image.onrender.com/api';
 
 export const authAPI = {
   register: async (username, password) => {
